6e8d0e79c0206bf04c885ba9f490200903db38b1
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2026-06-03 05:41:52 +1000

    this is the boilerplate for the code

--- calculator.py
+++ calculator.py
@@ -7,6 +7,14 @@ root = tk.Tk()
 root.title("Calculator")
 
 # creating the frames
+frame1 = tk.frame(root)
+frame1.pack(side='left', anchor='n')
+frame2 = tk.frame(root)
+frame2.pack(side='left', anchor='n')
+frame3 = tk.frame(root)
+frame3.pack(side='left', anchor='n')
+frame4 = tk.frame(root)
+frame4.pack(side='left', anchor='n')
 
 # keeps the window open
 root.mainloop
